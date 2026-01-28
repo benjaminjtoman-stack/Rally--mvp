65095a0f0f8d44658a3a313228f783173b07fc08
benjaminjtoman-stack <benjaminjtoman@gmail.com>
2026-01-28 10:28:12 +0000

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ maxWidth: 720, margin: "40px auto", fontFamily: "system-ui" }}>
-      <h1>Rally</h1>
-      <p><b>Play. Rank. Rise.</b></p>
-      <p>Rally Bristol pilot.</p>
-    </main>
-  );
-}
